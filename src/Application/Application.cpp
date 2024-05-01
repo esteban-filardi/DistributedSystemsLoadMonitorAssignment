b518ea86c763e7caaea4c7bb145205b086697fef
Esteban Filardi <1585383+esteban-filardi@users.noreply.github.com>
2024-05-01 20:53:46 -0300

    Refactor main function logic into an Application class

---
+++ src/Application/Application.cpp
@@ -0,0 +1,100 @@
+#include "Application.h"
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <chrono>
+#include <unistd.h>
+#include <csignal>
+#include <thread>
+#include <functional>
+#include "../LoadInfoReporter/LoadInfoReporter.h"
+#include "../GuidGenerator/GuidGenerator.h"
+
+char Application::_privateGroup[MAX_GROUP_NAME];
+char Application::_spreadName[80];
+mailbox Application::_mailbox;
+
+Application::Application() {
+    
+}
+
+int Application::Run(int argc, char* argv[]) {
+	SetUpStopSignalHandler();
+	ProcessParameters(argc, argv);
+
+	std::string user = GuidGenerator::GenerateGuid();
+
+	/* connecting to the daemon, requesting group information */
+	std::cout << "Connecting with username: " << user << std::endl;
+	int ret = SP_connect(_spreadName, user.c_str(), 0, 1, &_mailbox, _privateGroup);
+	if (ret < 0)
+	{
+		SP_error(ret);
+		exit(0);
+	}
+
+	/* joining a group */
+	std::cout << "Joining group: " << Application::CLUSTER_GROUP_NAME << std::endl;
+	SP_join(_mailbox, CLUSTER_GROUP_NAME.c_str());
+
+
+	auto sendLoadInfoBoundTask = std::bind(SendLoadInfoTask, _mailbox, user, CLUSTER_GROUP_NAME);
+	std::thread sendLoadInfoThread(sendLoadInfoBoundTask);
+	sendLoadInfoThread.join();
+	
+	return 0;
+}
+
+void SendLoadInfoTask(mailbox mailboxParam, std::string user, std::string clusterGroupName)
+{
+	LoadInfoReporter loadInfoReporter(mailboxParam, user, clusterGroupName);
+
+	while (true)
+	{
+		loadInfoReporter.SendLoadInfo();
+		
+		std::chrono::seconds durationToWait(1);
+		std::this_thread::sleep_for(durationToWait);
+	}
+}
+
+void Application::ProcessParameters(int argc, char* argv[])
+{
+	/* Setting defaults */
+	sprintf(this->_spreadName, "4803");
+	while (--argc > 0)
+	{
+		argv++;
+
+		if (!strncmp(*argv, "-s", 2)) {
+			strcpy(this->_spreadName, argv[1]);
+			argc--; argv++;
+		}
+		else {
+			printf("Usage: user\n%s\n",
+				"\t[-s <address>]    : either port or port@machine");
+			exit(1);
+		}
+	}
+}
+
+Application::~Application() {
+    SP_disconnect(Application::_mailbox);
+}
+
+void StopSignalHandler(int s)
+{
+	printf("\nCaught signal %d\n", s);
+	exit(1);
+}
+
+void Application::SetUpStopSignalHandler()
+{
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = StopSignalHandler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+}
---
+++ src/Application/Application.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include "../../include/sp.h"
+
+class Application {
+public:
+    static mailbox _mailbox;
+
+    Application();
+    ~Application();
+
+    int Run(int argc, char* argv[]);
+private:
+    const std::string CLUSTER_GROUP_NAME = "assignment";
+    /** Returned group name which can be used to send unicast messages to this connection (It is required for SP_Conect) */
+    static char _privateGroup[MAX_GROUP_NAME];
+    static char _spreadName[80];
+
+    void ProcessParameters(int argc, char* argv[]);
+    void SetUpStopSignalHandler();    
+};
+
+void StopSignalHandler(int s);
+void SendLoadInfoTask(mailbox mailboxParam, std::string user, std::string clusterGroupName);
